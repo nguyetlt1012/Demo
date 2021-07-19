745e728ff737ad62318a925c83a8558aafa90074
nguyetlt1012 <lenguyetds@gmail.com>
2021-07-19 21:44:30 +0700

    push code

---
+++ index.js
@@ -0,0 +1,20 @@
+let flashsaletime = 7200;
+
+function paddingtime(timeUnit) {
+  return timeUnit < 10 ? "0" + timeUnit : timeUnit;
+}
+function setCountDownFlashSale(second) {
+  if (second == 0) {
+    clearInterval(countDown);
+  }
+  var hours = Math.floor(second / 3600);
+  var minutes = Math.floor((second % 3600) / 60);
+  var seconds = second - hours * 3600 - minutes * 60;
+
+  $("#hour").text(paddingtime(hours));
+  $("#minute").text(paddingtime(minutes));
+  $("#second").text(paddingtime(seconds));
+}
+let countDown = setInterval(function () {
+  setCountDownFlashSale(flashsaletime--);
+}, 1000);
